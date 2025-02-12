9ba00b1c2c0af9d4eaa7905feea3a9789cad8dcd
namratha <namrathagaddad@gmail.com>
2025-02-12 21:17:28 +0100

    initial commit

---
+++ src/main/java/Newproject/HelloWorld.java
@@ -0,0 +1,12 @@
+package Newproject;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello There!!");
+	}
+		// TODO Auto-generated method stub
+
+	}
+
+
